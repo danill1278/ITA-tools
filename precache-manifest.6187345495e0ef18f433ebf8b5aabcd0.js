ab63cc7623580f515a456a050f8780a620eba92e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-09 07:59:52 +0000

    Deploy danill1278/ita-tools to github.com/danill1278/ita-tools.git:gh-pages

---
+++ precache-manifest.6187345495e0ef18f433ebf8b5aabcd0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9068785197e3d861446d36fbbd0582",
+    "url": "/ita-tools/index.html"
+  },
+  {
+    "revision": "2df5673934839f5ab1a9",
+    "url": "/ita-tools/static/css/main.b5c165bc.chunk.css"
+  },
+  {
+    "revision": "bb46e5077ea5fbf63f26",
+    "url": "/ita-tools/static/js/2.343bd4ea.chunk.js"
+  },
+  {
+    "revision": "2df5673934839f5ab1a9",
+    "url": "/ita-tools/static/js/main.f22f6b6e.chunk.js"
+  },
+  {
+    "revision": "b4324665e4db04ef27ad",
+    "url": "/ita-tools/static/js/runtime-main.62a55622.js"
+  },
+  {
+    "revision": "1a8bedae279e778d07a8739fc9e4fefa",
+    "url": "/ita-tools/static/media/icomoon.1a8bedae.woff"
+  },
+  {
+    "revision": "204584fe8b9b8cf2e371ed683cdc8aeb",
+    "url": "/ita-tools/static/media/icomoon.204584fe.ttf"
+  },
+  {
+    "revision": "3308d96a4540cef3bbafdd64abe7cead",
+    "url": "/ita-tools/static/media/icomoon.3308d96a.svg"
+  },
+  {
+    "revision": "e9b0b6b32c9aa431a2b5e16efd154972",
+    "url": "/ita-tools/static/media/icomoon.e9b0b6b3.eot"
+  }
+]);
