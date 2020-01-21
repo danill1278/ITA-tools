87feed2339ea640113939f052d20b2fcc749f542
yanosh-almashi <yanosh.almashi@gmail.com>
2020-01-21 15:22:01 +0200

    cleaned component

--- src/components/App/Loader/Loader.tsx
+++ src/components/App/Loader/Loader.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const Loader = () => {
-  return (
-    <div>
-      <CircularProgress />
-    </div>
-  );
-};
+const Loader = () => <CircularProgress />;
 
 export default Loader;
