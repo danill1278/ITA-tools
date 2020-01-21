84973fda005c7222b1b6b8a8e4c159c1a1524e22
yanosh-almashi <yanosh.almashi@gmail.com>
2020-01-21 14:13:03 +0200

    added Loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import "./App.css";
-import MenuContainer from "./MenuContainer/MenuContainer";
-import ContentWrapper from "./ContentWrapper/ContentWrapper";
-import AuthPopUp from "./Auth/Auth";
+import React from 'react';
+import './App.css';
+import MenuContainer from './MenuContainer/MenuContainer';
+import ContentWrapper from './ContentWrapper/ContentWrapper';
+import AuthPopUp from './Auth/Auth';
+import Loader from './Loader/Loader';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <AuthPopUp />
       <MenuContainer />
       <ContentWrapper />
